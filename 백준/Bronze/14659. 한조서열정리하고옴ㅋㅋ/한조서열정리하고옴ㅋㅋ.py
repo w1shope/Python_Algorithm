ba5245e1ad463d7a1d7e7f667429d8ec7f1ebd1b
w1shope <122812652+w1shope@users.noreply.github.com>
2023-01-19 22:49:37 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 88 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -1,14 +1,18 @@
-N = int(input())
+import sys
 
-player = [int(i) for i in input().split()]
-play_score = list()
-for i in range(len(player)) : 
+N = int(sys.stdin.readline())
+
+player = [int(i) for i in sys.stdin.readline().split()]
+best_score = 0
+for i in range(N) : 
+    if(len(range(i, N)) <= best_score) :
+        break
     score = 0
-    for j in range(i, len(player)) :
+    for j in range(i, N) :
         if(player[i] > player[j]) :
             score += 1
         elif(player[i] < player[j]) :
             break
-    play_score.append(score)
+    best_score = max(best_score, score)
 
-print(max(play_score))
+print(best_score)
