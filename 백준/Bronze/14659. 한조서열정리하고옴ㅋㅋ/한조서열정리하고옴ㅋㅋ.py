5baa44e26c9b164fadba357e711b45b82266479c
w1shope <122812652+w1shope@users.noreply.github.com>
2023-01-19 22:52:49 +0900

    Update 한조서열정리하고옴ㅋㅋ.py
    주석 추가

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -2,9 +2,10 @@ import sys
 
 N = int(sys.stdin.readline())
 
-player = [int(i) for i in sys.stdin.readline().split()]
+player = [int(i) for i in sys.stdin.readline().split()] 
 best_score = 0
 for i in range(N) : 
+    # 현재 활잡이가 처치한 적의 수 > 남은 활잡이 수이면 더 이상 탐색할 필요가 없다
     if(len(range(i, N)) <= best_score) :
         break
     score = 0
